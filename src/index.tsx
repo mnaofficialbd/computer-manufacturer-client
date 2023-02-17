15afaa485346224e40b30ff1a032677905db889c
mnaofficial <mna.official92@gmail.com>
2023-02-17 12:19:51 +0600

    index file setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation"
+import { HelmetProvider } from 'react-helmet-async';
+
+// Create a client
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <HelmetProvider>
   <BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </React.StrictMode>
   </BrowserRouter>
+  </HelmetProvider>
 );
 
 reportWebVitals();

8a38ad867d775f7cbf631a02b9cd10d2b289c476
mnaofficial <mna.official92@gmail.com>
2023-02-20 10:58:50 +0600

    Frequescy component setup

--- src/Components/Home/Frequescy.tsx
+++ src/Components/Home/Frequescy.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Accordion from './Accordion';
+import VideoFrame from './VideoFrame';
+import {faqsData} from '../Shared/Datas';
 
 const Frequescy = () => {
+    const [active, setActive] = useState(1)
     return (
-        <div>
-            
-        </div>
+        <section className=''>
+            <div className='text-center mb-6'>
+                <h2 className='text-4xl mb-2 text-orange-500 font-bold'>Frequency</h2>
+                <h4 className='text-2xl'>Your frequency question & answer</h4>
+            </div>
+            <div className='flex_block'>
+                <div className=''>
+                    <p className='text-center mb-5'>MNA Motivation Youtube Channel intro Video</p>
+                    <VideoFrame />
+                </div>
+                <div className='' >
+                    {
+                        faqsData.map(faq=>(
+                            <Accordion 
+                            key={faq.id} id={faq.id} 
+                            title={faq.title} ans={faq.ans} 
+                            active={active} setActive={setActive} />
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
     );
 };
 

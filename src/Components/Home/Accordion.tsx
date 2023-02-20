cc2e160e72c4e19e0d505a59cfc0c37ba42e50ef
mnaofficial <mna.official92@gmail.com>
2023-02-20 10:45:20 +0600

    Accordion component setup

--- src/Components/Home/Accordion.tsx
+++ src/Components/Home/Accordion.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import '../../style/Accordion.css';
 
-const Accordion = () => {
+const Accordion = ({ id, title, ans, active, setActive }) => {
+
     return (
-        <div>
-            
-        </div>
+        <section className=''>
+            <div className="accordion">
+                <div className="accordionHeading">
+                    <div className="containerBox">
+                        <p>{title}</p>
+                        <span onClick={() => setActive(id)}>
+                            {active===id? "X": "|||"}
+                        </span>
+                    </div>
+                </div>
+                <div className={(active === id ? "show" : "") + " accordionContent"}>
+                    <div className="containerBox">
+                        <p>{ans}</p>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 

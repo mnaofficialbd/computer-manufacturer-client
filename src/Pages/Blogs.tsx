7666ab4bea026be22247a87bcc64b8f6e715e92a
mnaofficial <mna.official92@gmail.com>
2023-02-24 19:37:27 +0600

    Blogs page setup

--- src/Pages/Blogs.tsx
+++ src/Pages/Blogs.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Blog from '../Components/Blogs/Blog';
+import PageTitle from '../Components/Shared/PageTitle';
 
 const Blogs = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+        <PageTitle title='Blogs'></PageTitle>
+            <Blog />
+        </>
     );
 };
 

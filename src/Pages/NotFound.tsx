0b9aeebbe9a717a30960b08f9f8022376c6c6cd4
mnaofficial <mna.official92@gmail.com>
2023-02-24 19:52:41 +0600

    404 page setup

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import PageTitle from '../Components/Shared/PageTitle';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='mt-12'>
+            <PageTitle title='4 0 4'></PageTitle>
+            <img src="https://i.ibb.co/q5F5b5v/404.png" alt="" className="w-full" />
         </div>
     );
 };
